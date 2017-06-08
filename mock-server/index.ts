f2e97ac833cc3afb28fbf1adcb67b6586f1bea14
tomermoshe <152430tm>
2017-06-08 13:28:39 +0300

    spacing

--- mock-server/index.ts
+++ mock-server/index.ts
@@ -16,6 +16,7 @@ const WS_GQL_PATH = '/subscriptions';
 
 const app = express();
 app.use(cors());
+
 app.use('/graphql', bodyParser.json(), graphqlExpress({
   schema,
   debug: true,
@@ -25,8 +26,8 @@ app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
 
-
 const server = createServer(app);
+
 new SubscriptionServer(
   {
     schema,
