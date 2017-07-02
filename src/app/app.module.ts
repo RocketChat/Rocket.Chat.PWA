43638a82c9edcdfb07e1732c58f3f25701003bd2
InsaneNaman <namangupta.cse@gmail.com>
2017-07-02 22:21:46 +0530

    initialised and added teradata covalent dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule,JsonpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { ChannelListComponent } from './components/channel-list/channel-list.component';
+
+// covalent modules
+import {CovalentLayoutModule, CovalentStepsModule} from '@covalent/core';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { ChannelListComponent } from './components/channel-list/channel-list.com
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,JsonpModule
+    HttpModule, JsonpModule,
+    CovalentLayoutModule, CovalentStepsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
