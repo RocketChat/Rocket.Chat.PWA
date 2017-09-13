e97b09ab076e6c32b2905cffe15644b9ca47e16d
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-09-13 17:43:21 +0200

    Remove tests

--- src/app/shared/pipes/default-avatar/default-avatar.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultAvatarPipe } from './default-avatar.pipe';
-
-describe('DefaultAvatarPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DefaultAvatarPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
