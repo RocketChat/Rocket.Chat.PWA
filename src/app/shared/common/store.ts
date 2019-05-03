03af28f44afd0567c8e9100c2006dadf0b6d249a
mayur-rawte <rawte.mayur@gmail.com>
2019-05-03 19:09:20 +0530

    Fixed reduxPersist error on app start

--- src/app/shared/common/store.ts
+++ src/app/shared/common/store.ts
@@ -2,7 +2,7 @@ import { getApolloClient } from '../../graphql/client/apollo-client';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { reducer as accounts } from '@accounts/client';
 import { persistStore, autoRehydrate } from 'redux-persist';
-import immutableTransform from 'redux-persist-transform-immutable';
+import * as immutableTransform from 'redux-persist-transform-immutable';
 
 const apolloClient = getApolloClient();
 
